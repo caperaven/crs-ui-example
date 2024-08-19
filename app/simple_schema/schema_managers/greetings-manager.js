e272b0a896074593491d0f12a29e238eb59406f3
caperaven <caperaven@gmail.com>
2024-08-19 15:02:12 +0200

    initial

--- app/simple_schema/schema_managers/greetings-manager.js
+++ app/simple_schema/schema_managers/greetings-manager.js
@@ -5,6 +5,10 @@ export class GreetingsManager {
         return true;
     }
 
+    /**
+     * Mark this manager as a value processor so that the provider knows to call the process method.
+     * @returns {boolean}
+     */
     get valueProcessor() {
         return true;
     }
@@ -31,6 +35,14 @@ export class GreetingsManager {
         this.#data = arg;
     }
 
+    /**
+     * This is called when the provider calls the parseStringValue method
+     * See the heading provider for an example of how this is used.
+     * 1. Evaluate if the value matches the markers you are looking for.
+     * 2. Return the updated string with the required modifications.
+     * @param value
+     * @returns {Promise<*|string>}
+     */
     async process(value) {
         if (typeof value != "string" || !value.startsWith("$g{")) return value;
         // strip value from between $g{ and }
